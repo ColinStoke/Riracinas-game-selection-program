3dc88fecdf3d92216785d70a88a0070291947346
ColinStoke <colinstokes.business@gmail.com>
2025-06-25 16:11:08 -0400

    finished basic selection code, need to work on the code that unlocks games based on finishes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Riracina's 3ds marathon game picker program
 
+    Author: Colin Stokes
+
 ## Description
 
     Riracina needs a program that returns an amount of games from the total games list that aren't locked or finished. This program manages the games list and returns random games for her.
--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 
 import random
 
+#function that prints the main menu of the program
 def print_menu():
 
     print("----------Welcome to Riracina's Game Picker and Tracker!----------\n\nWhat would you like to do?\n1. Select a subset of games\n2. Finish a game\n\nType \"exit\" to exit the program.")
+
+#function that prints the game selection
+def print_selected_games(selected_games : list):
+    print(f"\nYour {len(selected_games)} selected games:\n-------------------------")
+
+    for game in selected_games:
+        print(game)
+    
+    print("-------------------------\n")
             
 
 if __name__ == '__main__':
@@ -17,11 +27,13 @@ if __name__ == '__main__':
     locked_status : str #status of the game extracted from the total_games file
 
     exit_trigger : bool = False #trigger boolean to end the program
+    valid_integer : bool = False #trigger boolean to end the selection number loop
 
     #open the file and read all lines
     with open("Total_Games_List.txt") as total_games:
 
         all_lines = total_games.readlines() #read all the lines into a list 
+        total_games.close()
 
     for full_line in all_lines:
 
@@ -34,8 +46,6 @@ if __name__ == '__main__':
 
         if(locked_status == "unlocked"):
             unlocked_games.append(game_name)
-        
-    #print(unlocked_games)
 
     while(not exit_trigger):
 
@@ -49,10 +59,21 @@ if __name__ == '__main__':
             print("\nyou chose select!\n")
             random.seed() #set seed to current system time or whatever your os feels like is best
 
-            selected_games = random.sample(unlocked_games, 20) #generate 20 random games from the list of unlocked games
+            valid_integer = False
 
-            for game in selected_games:
-                print(game)
+            #loop to keep prompting until a valid integer is inputted
+            while(not valid_integer):
+                try:
+
+                    select_num = int(input("How many games would you like to select?\n>> ")) #throws an error if a non-integer is passed
+                    valid_integer = True
+
+                except: #catches the error
+                    print("\n!!---Invalid input. Please pass an integer value to the program---!!\n")
+
+            selected_games = random.sample(unlocked_games, select_num) #generate an amount of random games from the list of unlocked games
+
+            print_selected_games(selected_games)
 
         elif(choice == "2" or choice == "finish"):
             print("\nyou chose finish!\n")
