7a9236423785e2a45f01d065c30d888fd484a6b3
ColinStoke <colinstokes.business@gmail.com>
2025-06-26 21:48:02 -0400

    finished by fixing some bugs and adding the feature that allows for multiple games to be unlocked by one game. Also fixed a bug where a game would unlock itself by fixing some double jeapordy on line 141

--- main.py
+++ main.py
@@ -28,12 +28,14 @@ if __name__ == '__main__':
     prereq_games = list() #list of all prereq games
     newly_unlocked_games = list() #list of all newly unlocked games
     prereq_list = list() #list of all prereqs
+    prereq_list_index = list()
+    AND_flag_list = list()
+    other_prereq_list = list()
 
     game_name : str #name of the game extracted from any games file
     locked_status : str #status of the game extracted from the total_games file
     prereq_game : str #name of prereq game
     finished_game : str #name of finished game
-    other_prereq : str #string variable that holds the other prereq
 
     exit_trigger : bool = False #trigger boolean to end the program
     valid_integer : bool = False #trigger boolean to end the selection number loop
@@ -43,29 +45,31 @@ if __name__ == '__main__':
     is_prereq : bool = False #boolean that denotes whether finished game is a prereq or not
 
     counter : int #counter variable
-    prereq_list_index : int
-
-    #open the file and read all lines
-    with open("Total_Games_List.txt") as total_games_file:
-
-        total_games_all_lines = total_games_file.readlines() #read all the lines into a list 
-        total_games_file.close()
-
-    for full_line in total_games_all_lines:
-
-        splitLine = full_line.split(", ") #splits the line into name of game and locked or unlocked status
-
-        game_name = splitLine[0]
-        locked_status = splitLine[1]
-
-        total_games_list.append(game_name)
-
-        locked_status = locked_status.strip("\n") #get rid of trailing newline character
-
-        if(locked_status == "unlocked"):
-            unlocked_games.append(game_name)
 
     while(not exit_trigger):
+
+        unlocked_games.clear()
+        total_games_list.clear()
+
+        #open the file and read all lines
+        with open("Total_Games_List.txt", "r", encoding="utf-8") as total_games_file:
+
+            total_games_all_lines = total_games_file.readlines() #read all the lines into a list 
+            total_games_file.close()
+
+        for full_line in total_games_all_lines:
+
+            splitLine = full_line.split(", ") #splits the line into name of game and locked or unlocked status
+
+            game_name = splitLine[0]
+            locked_status = splitLine[1]
+
+            total_games_list.append(game_name)
+
+            locked_status = locked_status.strip("\n") #get rid of trailing newline character
+
+            if(locked_status == "unlocked"):
+                unlocked_games.append(game_name)
 
         print_menu() #print the program menu
 
@@ -95,12 +99,12 @@ if __name__ == '__main__':
 
         elif(choice == "2" or choice == "finish"):
 
+            prereq_list.clear()
+
             #read all lines into a list
-            with open("Locked_Games_List.txt") as locked_file:
+            with open("Locked_Games_List.txt", "r", encoding="utf-8") as locked_file:
                 locked_games_all_lines = locked_file.readlines()
                 locked_file.close()
-
-            AND_flag = False
             
             #build the locked games dictionary
             for full_line in locked_games_all_lines:
@@ -125,70 +129,83 @@ if __name__ == '__main__':
                     print(f"Congrats on completing {finished_game}!\n")
                     valid_game = True
 
-                    with open("Finished_Games_List.txt", "a") as finished_file:
+                    with open("Finished_Games_List.txt", "a", encoding="utf-8") as finished_file:
                         finished_file.write(finished_game + "\n")
                         finished_file.close()
 
+                    prereq_list_index.clear()
+                    other_prereq_list.clear()
+                    AND_flag_list.clear()
                     counter = 0
                     is_prereq = False
                     while(counter < len(prereq_list)):
                         split2 = prereq_list[counter].split("++")
                         if(finished_game in split2):
-                            prereq_list_index = counter
+                            prereq_list_index.append(counter)
                             is_prereq = True
                             if len(split2) > 1:
-                                AND_flag = True
+                                AND_flag_list.append(True)
                                 if finished_game == split2[0]:
-                                    other_prereq = split2[1]
+                                    other_prereq_list.append(split2[1])
                                 else:
-                                    other_prereq = split2[0]
-                            break
-                        
-                        split2 = prereq_list[counter].split("|")
-                        if(finished_game in split2):
-                            prereq_list_index = counter
-                            is_prereq = True
-
+                                    other_prereq_list.append(split2[0])
+                            else:
+                                print("testing")
+                                print(len(prereq_list_index))
+                                AND_flag_list.append(False)
+                        else:
+                            split2 = prereq_list[counter].split("|")
+                            if(finished_game in split2):
+                                prereq_list_index.append(counter)
+                                is_prereq = True
+                                AND_flag_list.append(False)
+                    
                         counter += 1
                     
                     if is_prereq:
+
+                        for i in range(len(prereq_list_index)):
+
+                            game_name = locked_games_all_lines[prereq_list_index[i]].split(", ")[0]
                         
-                        game_name = locked_games_all_lines[prereq_list_index].split(", ")[0]
+                            if AND_flag_list[i]:
+                                locked_games_all_lines[prereq_list_index[i]] = f"{game_name}, {other_prereq_list[i]}\n"
+                            else:
+                                locked_games_all_lines.pop(prereq_list_index[i])
+
+                                if(i + 1 in range(len(prereq_list_index))):
+                                    prereq_list_index[i + 1] -= 1
+
+                            found_line = True
+
+                            if not AND_flag_list[i]:
+                                print(f"~~~~~~~~~~You unlocked {game_name}!~~~~~~~~~~\n")
+
+                                found_line = False
+                                counter = 0
+
+                                while(not found_line):
+
+                                    if total_games_list[counter] == game_name:
+                                        total_games_all_lines[counter] = f"{game_name}, unlocked\n"
+                                        found_line = True
+
+                                    counter += 1
+
+
+                        with open("Locked_Games_List.txt", "w", encoding="utf-8") as locked_games_file:
+                                locked_games_file.writelines(locked_games_all_lines)
+
+                        with open("Total_Games_List.txt", "w", encoding="utf-8") as total_games_file:
+                                    total_games_file.writelines(total_games_all_lines)
                         
-                        if AND_flag:
-                            locked_games_all_lines[prereq_list_index] = f"{game_name}, {other_prereq}\n"
-                        else:
-                            locked_games_all_lines.pop(prereq_list_index)
-
-                        found_line = True
-
-                        with open("Locked_Games_List.txt", "w") as locked_games_file:
-                            locked_games_file.writelines(locked_games_all_lines)
-
-                        print(f"\n----------You unlocked {game_name}!----------\n")
-
-                        found_line = False
-                        counter = 0
-
-                        while(not found_line):
-
-                            if total_games_list[counter] == game_name:
-                                total_games_all_lines[counter] = f"{game_name}, unlocked\n"
-                                found_line = True
-
-                            counter += 1
-
-                        with open("Total_Games_List.txt", "w") as total_games_file:
-                            total_games_file.writelines(total_games_all_lines)
-                            
-
                 else:
                     print(f"\nInvalid game name, consider copy pasting the game directly from the spreadsheet.\nlink: {SPREADSHEET_LINK}\n")
 
 
                 
         elif(choice == "exit"):
-            print("\nyou chose to exit!\n")
+            print("\nHave a good day! Byeeee!\n")
             exit_trigger = True
         else:
             print("Invalid input. See the Usage section in the readme for more details")
@@ -196,5 +213,4 @@ if __name__ == '__main__':
         #TODO: write Usage section in the readme
         #      Code logic that unlocks the games when the finish option is selected
         #      Make a GUI???? Maybe
-        #      Code select logic to return a specified amount of games
         #      Make the program epic and professional and not with debug stuff everywhere
